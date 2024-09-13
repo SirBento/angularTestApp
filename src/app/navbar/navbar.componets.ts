e80ed288f37f76d2f606c2b626bbfb04c7d20e04
Bernard Towindo <bernardtowindo38@gmail.com>
2024-09-13 14:02:41 +0200

    Initial Commit

---
+++ src/app/navbar/navbar.componets.ts
@@ -0,0 +1,13 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:3822982464.
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+  
+})
+export class NavbarComponent {
+
+}
